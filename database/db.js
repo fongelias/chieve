52915292a7542ef01449c0b6618c1722f938e873
fongelias <fongelias@gmail.com>
2016-08-10 22:09:04 -0400

    added files

---
+++ database/db.js
@@ -0,0 +1,43 @@
+var promise = require('bluebird');
+
+var options = {
+	// Initialization Options
+	promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db
+
+// Connect to the database before starting the application server. 
+pg.connect(process.env.DATABASE_URL, function(err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+});
+
+module.exports = {
+	getAllUsers: getAllUsers
+};
+
+function getAllUsers(req, res, next) {
+  db.any('select * from users')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL users'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
