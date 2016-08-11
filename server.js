376ba500274181fc1bffccd1e66ed432b8e5edc6
fongelias <fongelias@gmail.com>
2016-08-10 22:01:07 -0400

    testing bluebird framework

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
 var express = require("express"); //Node module
 var path = require("path");
-var pg = require("pg"); //Node module
+var router = express.Router();
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
 
-// Connect to the database before starting the application server. 
-pg.connect(process.env.DATABASE_URL, function(err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+var db = require('database/db.js');
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+var port = server.address().port;
+console.log("App now running on port", port);
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
 
 //Placeholder Password: 5f4dcc3b5aa765d61d8327deb882cf99
 
@@ -41,14 +29,4 @@ function handleError(res, reason, message, code) {
  *    GET: finds all contacts
  *    POST: creates a new contact
  */
-
- app.get("/contacts", function (request, response) {
-    db.client.query('SELECT * FROM users', function(err, result) {
-      done();
-      if (err)
-       { handleError(res, err.message, "Failed to get contacts."); }
-      else
-       { response.send("hey man") }
-    });
-
-});
+router.get('/contacts', db.getAllUsers);
