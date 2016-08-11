0d17e5f48a7fc11534bce71e2e125319701a77ff
fongelias <fongelias@gmail.com>
2016-08-10 20:34:40 -0400

    Testing API endpoint

--- server.js
+++ server.js
@@ -25,3 +25,31 @@ pg.connect(process.env.DATABASE_URL, function(err, database) {
     console.log("App now running on port", port);
   });
 });
+
+//Placeholder Password: 5f4dcc3b5aa765d61d8327deb882cf99
+
+
+// LOGIN API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+ app.get("/", function (request, response) {
+
+    db.query('SELECT * FROM users', function(err, result) {
+      done();
+      if (err)
+       { handleError(res, err.message, "Failed to get contacts."); }
+      else
+       { res.status(200).send('result'); }
+    });
+
+});
